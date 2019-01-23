73c0df0ae860d9b2140bd968412671af795c7b48
Nat Grant <email.natgrant@gmail.com>
2019-01-23 17:45:38 +1300

    tidying up

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const dataPath = "./data.json";
+const bodyparser = require("body-parser");
 
 router.get("/", (req, res) => {
   res.render(__dirname + "/views/home");
